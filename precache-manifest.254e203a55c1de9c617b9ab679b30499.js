0606a5b43f7f3de76c5888fdf5afe2cbf3acf33a
Yuriy Bahur <yuriybahur1@gmail.com>
2019-02-11 19:24:13 +0200

    Updates

---
+++ precache-manifest.254e203a55c1de9c617b9ab679b30499.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e31e6278da48539f97e8",
+    "url": "/adopt/static/js/runtime~main.e31e6278.js"
+  },
+  {
+    "revision": "cccbf9779da3860b004c",
+    "url": "/adopt/static/js/main.cccbf977.chunk.js"
+  },
+  {
+    "revision": "48477336be3776ab04d2",
+    "url": "/adopt/static/js/1.48477336.chunk.js"
+  },
+  {
+    "revision": "cccbf9779da3860b004c",
+    "url": "/adopt/static/css/main.e48dd036.chunk.css"
+  },
+  {
+    "revision": "a53b2e9b670e87aa58ae12d40ca94339",
+    "url": "/adopt/index.html"
+  }
+];
